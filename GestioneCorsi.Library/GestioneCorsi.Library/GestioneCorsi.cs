c66f9d09a36aae8808dd3a3dd5c16d5d9579e0ca
Cheemscool <75301496+Cheemscool@users.noreply.github.com>
2022-03-01 08:31:29 +0100

    Libreria
    Correzione libreria.

---
+++ GestioneCorsi.Library/GestioneCorsi.Library/GestioneCorsi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneCorsi.Library
+{
+    public class GestioneCorsi
+    {
+        public List<Corso> Corsi { get; }
+
+        public void AggiungiCorso(Corso corso)
+        {
+            Corsi.Add(corso);
+        }
+    }
+}
---
+++ GestioneCorsi.Library/GestioneCorsi.Library/Studente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneCorsi.Library
+{
+    public class Studente : Persona
+    {
+        public Studente(string Nome, string Cognome, string Matricola) : base(Nome, Cognome)
+        {
+            this.Matricola = Matricola;
+        }
+        public string Matricola { get; }
+    }
+}
