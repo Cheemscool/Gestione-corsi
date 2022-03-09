a31f594e8bcdc29e914ee88043d2ab5df9d2b06b
Vigno04 <75776605+Vigno04@users.noreply.github.com>
2022-03-09 11:49:32 +0100

    New Form
    Aggiunta nuova form per le informazioni del corso

---
+++ VignaliDavide_AlejandroDeniel_GestioneCorsi/VignaliDavide_AlejandroDeniel_GestioneCorsi/FrmInfoCorso.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace VignaliDavide_AlejandroDeniel_GestioneCorsi
+{
+    partial class FrmInfoCorso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblNomeCorso = new System.Windows.Forms.Label();
+            this.LstStudenti = new System.Windows.Forms.ListBox();
+            this.LstDocenti = new System.Windows.Forms.ListBox();
+            this.LstLezioni = new System.Windows.Forms.ListBox();
+            this.LstAule = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LblEdizione = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LblNomeCorso
+            // 
+            this.LblNomeCorso.AutoSize = true;
+            this.LblNomeCorso.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblNomeCorso.Location = new System.Drawing.Point(115, 9);
+            this.LblNomeCorso.Name = "LblNomeCorso";
+            this.LblNomeCorso.Size = new System.Drawing.Size(294, 65);
+            this.LblNomeCorso.TabIndex = 0;
+            this.LblNomeCorso.Text = "Nome Corso";
+            // 
+            // LstStudenti
+            // 
+            this.LstStudenti.FormattingEnabled = true;
+            this.LstStudenti.ItemHeight = 15;
+            this.LstStudenti.Location = new System.Drawing.Point(12, 113);
+            this.LstStudenti.Name = "LstStudenti";
+            this.LstStudenti.Size = new System.Drawing.Size(120, 319);
+            this.LstStudenti.TabIndex = 1;
+            // 
+            // LstDocenti
+            // 
+            this.LstDocenti.FormattingEnabled = true;
+            this.LstDocenti.ItemHeight = 15;
+            this.LstDocenti.Location = new System.Drawing.Point(138, 113);
+            this.LstDocenti.Name = "LstDocenti";
+            this.LstDocenti.Size = new System.Drawing.Size(120, 319);
+            this.LstDocenti.TabIndex = 2;
+            // 
+            // LstLezioni
+            // 
+            this.LstLezioni.FormattingEnabled = true;
+            this.LstLezioni.ItemHeight = 15;
+            this.LstLezioni.Location = new System.Drawing.Point(264, 113);
+            this.LstLezioni.Name = "LstLezioni";
+            this.LstLezioni.Size = new System.Drawing.Size(247, 139);
+            this.LstLezioni.TabIndex = 3;
+            // 
+            // LstAule
+            // 
+            this.LstAule.FormattingEnabled = true;
+            this.LstAule.ItemHeight = 15;
+            this.LstAule.Location = new System.Drawing.Point(264, 278);
+            this.LstAule.Name = "LstAule";
+            this.LstAule.Size = new System.Drawing.Size(247, 154);
+            this.LstAule.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(415, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Edizione:";
+            // 
+            // LblEdizione
+            // 
+            this.LblEdizione.AutoSize = true;
+            this.LblEdizione.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblEdizione.Location = new System.Drawing.Point(438, 47);
+            this.LblEdizione.Name = "LblEdizione";
+            this.LblEdizione.Size = new System.Drawing.Size(43, 17);
+            this.LblEdizione.TabIndex = 6;
+            this.LblEdizione.Text = "label3";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl1.Location = new System.Drawing.Point(12, 89);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(116, 21);
+            this.lbl1.TabIndex = 7;
+            this.lbl1.Text = "Studenti iscritti:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(138, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Personale:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(264, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Lezioni in corso:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(264, 255);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 21);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Aule attive::";
+            // 
+            // FrmInfoCorso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 444);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.LblEdizione);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LstAule);
+            this.Controls.Add(this.LstLezioni);
+            this.Controls.Add(this.LstDocenti);
+            this.Controls.Add(this.LstStudenti);
+            this.Controls.Add(this.LblNomeCorso);
+            this.Name = "FrmInfoCorso";
+            this.Text = "FrmInfoCorso";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblNomeCorso;
+        private System.Windows.Forms.ListBox LstStudenti;
+        private System.Windows.Forms.ListBox LstDocenti;
+        private System.Windows.Forms.ListBox LstLezioni;
+        private System.Windows.Forms.ListBox LstAule;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblEdizione;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ VignaliDavide_AlejandroDeniel_GestioneCorsi/VignaliDavide_AlejandroDeniel_GestioneCorsi/FrmInfoCorso.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GestioneCorsi.Library;
+
+namespace VignaliDavide_AlejandroDeniel_GestioneCorsi
+{
+    public partial class FrmInfoCorso : Form
+    {
+        public FrmInfoCorso(List<Lezione> Lezioni,List<Studente> Studenti,List<Docente> Docenti,List<Aula> Aule,string Nome,int Edizione)
+        {
+            InitializeComponent();
+            LblEdizione.Text = Edizione.ToString();
+            LblNomeCorso.Text = Nome.ToString();
+
+            LstLezioni.DataSource = null;
+            LstStudenti.DataSource = null;
+            LstDocenti.DataSource = null;
+            LstAule.DataSource = null;
+
+            LstLezioni.DataSource= Lezioni;
+            LstStudenti.DataSource= Studenti;
+            LstDocenti.DataSource= Docenti;
+            LstAule.DataSource= Aule;
+        }
+    }
+}
