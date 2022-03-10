982d024afdb008dda68049977dc64e265d41bd54
Cheemscool <75301496+Cheemscool@users.noreply.github.com>
2022-03-10 08:50:48 +0100

    Progetto principale
    Continuato creazione dei corsi.

--- VignaliDavide_AlejandroDeniel_GestioneCorsi/VignaliDavide_AlejandroDeniel_GestioneCorsi/FrmAggiungiCorso.cs
+++ VignaliDavide_AlejandroDeniel_GestioneCorsi/VignaliDavide_AlejandroDeniel_GestioneCorsi/FrmAggiungiCorso.cs
@@ -23,14 +23,31 @@ namespace GestioneCorsi.Library
                 ckdLstBoxLezioni.Items.Add(lezioni);
             ckdLstBoxLezioni.DataSource = null;
             ckdLstBoxLezioni.DataSource = gestioneCorsi.Lezioni;
+
+            foreach (Studente studenti in gestioneCorsi.Studenti)
+                ckdLstBoxStudenti.Items.Add(studenti);
+            ckdLstBoxStudenti.DataSource = null;
+            ckdLstBoxStudenti.DataSource = gestioneCorsi.Studenti;
+
+            foreach (Docente docente in gestioneCorsi.Docenti)
+                ckdLstBoxDocenti.Items.Add(docente);
+            ckdLstBoxDocenti.DataSource = null;
+            ckdLstBoxDocenti.DataSource = gestioneCorsi.Docenti;
+
+            foreach (Aula aula in gestioneCorsi.Aule)
+                ckdLstBoxAule.Items.Add(aula);
+            ckdLstBoxAule.DataSource = null;
+            ckdLstBoxAule.DataSource = gestioneCorsi.Aule;
         }
 
         private void btnAggiungiCorso_Click(object sender, EventArgs e)
         {
-            if (txtBoxNome.Text == "" || txtBoxEdizione.Text == "" || ckdLstBoxLezioni.CheckedItems == null)
+            if (txtBoxNome.Text == "" || txtBoxEdizione.Text == "" || ckdLstBoxLezioni.CheckedItems == null || ckdLstBoxStudenti.CheckedItems == null || ckdLstBoxDocenti.CheckedItems == null || ckdLstBoxAule.CheckedItems == null)
                 MessageBox.Show("Per procedere devi compilare tutti i campi.");
+            else if (txtBoxEdizione.Text != )
             else
             {
+                Corso corso = new Corso(txtBoxNome.Text, int.Parse(txtBoxEdizione.Text), ckdLstBoxLezioni.CheckedItems, ckdLstBoxStudenti.CheckedItems, ckdLstBoxDocenti.CheckedItems, ckdLstBoxAule.CheckedItems);
                 Close();
             }
         }
